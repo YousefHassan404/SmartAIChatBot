aa85ebff22f3dbb7055cb8fdbd3e7ecd2ee2da02
YousefHassan404 <aaa01095836766@gmail.com>
2025-04-16 11:49:45 +0200

    added webhook api to the program

--- api/index.js
+++ api/index.js
@@ -10,5 +10,21 @@ app.get("/", (req, res) => {
   res.send("No Risk No Fun Yaaa Maجdy 😂");
 });
 
+app.get("/webhook",(req,res)=>{
+    const PAGE_VERIFICATION_TOKEN = "my_super_secret_token";
+
+    let token = req.query["hub.verify_token"];
+    let challenge = req.query["hub.challenge"];
+
+    if (token === PAGE_VERIFICATION_TOKEN) {
+        console.log("Token is valid, sending challenge back to Facebook.");
+        res.status(200).send(challenge);
+    } else {
+        console.log("Token is invalid, not sending challenge back to Facebook.");
+        res.status(403).send("Forbidden");
+    }
+
+})
+
 
 export default app;
