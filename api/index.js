fb0d8ffecd30612fdf96c78a4863fe66e27adf53
YousefHassan404 <aaa01095836766@gmail.com>
2025-04-17 08:37:22 +0200

    added privacy.html file

--- api/index.js
+++ api/index.js
@@ -1,30 +1,47 @@
 import express from "express";
 import bodyParser from "body-parser";
+import path from "path";
+import { fileURLToPath } from "url";
 
 const app = express();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
+// Middleware
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
+// Root route
 app.get("/", (req, res) => {
   res.send("No Risk No Fun Yaaa Maجdy 😂");
 });
 
-app.get("/webhook",(req,res)=>{
-    const PAGE_VERIFICATION_TOKEN = "my_super_secret_token";
+// Static files
+app.use(express.static(path.join(__dirname, "..", "public")));
 
-    let token = req.query["hub.verify_token"];
-    let challenge = req.query["hub.challenge"];
+// Privacy policy route
+app.get("/privacy", (req, res) => {
+  res.sendFile(path.join(__dirname, "..", "public", "privacy.html"));
+});
 
-    if (token === PAGE_VERIFICATION_TOKEN) {
-        console.log("Token is valid, sending challenge back to Facebook.");
-        res.status(200).send(challenge);
-    } else {
-        console.log("Token is invalid, not sending challenge back to Facebook.");
-        res.status(403).send("Forbidden");
-    }
+// Webhook verification
+app.get("/webhook", (req, res) => {
+  const PAGE_VERIFICATION_TOKEN = "my_super_secret_token";
+  let token = req.query["hub.verify_token"];
+  let challenge = req.query["hub.challenge"];
 
-})
+  if (token === PAGE_VERIFICATION_TOKEN) {
+    console.log("Token is valid, sending challenge back to Facebook.");
+    res.status(200).send(challenge);
+  } else {
+    console.log("Token is invalid, not sending challenge back to Facebook.");
+    res.status(403).send("Forbidden");
+  }
+});
 
+
+// app.listen(3000, () => {
+//     console.log('Server running on port 3000');
+//   });
 
 export default app;
