171324d8c6524a4f6c0eaa445cd23c8ad413dd77
YousefHassan404 <aaa01095836766@gmail.com>
2025-04-17 10:11:47 +0200

    added greeting text function

--- api/index.js
+++ api/index.js
@@ -4,6 +4,13 @@ import path from "path";
 import { fileURLToPath } from "url";
 
 const app = express();
+
+
+const PAGE_ACCESS_TOKEN =
+  "EAAN2h5lZBP9YBO8rpvOk6DmzpMIWqVZBPv3z7ZBmpGcfd6Fs7FpvCKXLrbEV0uC27L0ITv3ctlPX3SfNAi9rNPISevt7YzWBiw1ZB9BeFIeEZAhJtVHTnRckWXfjMlz5eXC7dJixpp4exB8yCxC2eQxcjfYWnq3uaT6KwkcpDZApuou962UvzsqGfYrimcKc1pywZDZD";
+
+  const graph_api =
+  `https://graph.facebook.com/v7.0/me/messenger_profile?access_token=${PAGE_ACCESS_TOKEN}`;
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
@@ -39,6 +46,70 @@ app.get("/webhook", (req, res) => {
   }
 });
 
+app.use("/setup" , (req ,res)=>{
+    // var data= {
+    //     "get_started": {
+    //         "payload": "GET_STARTED_PAYLOAD"
+    //     },
+    //     "greeting": [
+    //         {
+    //             "locale": "default",
+    //             "text": "Hello {{user_first_name}}!"
+    //         }
+    //     ],
+    //     "persistent_menu": [
+    //         {
+    //             "locale": "default",
+    //             "composer_input_disabled": false,
+    //             "call_to_actions": [
+    //                 {
+    //                     "title": "Start",
+    //                     "type": "postback",
+    //                     "payload": "START_PAYLOAD"
+    //                 },
+    //                 {
+    //                     "title": "Help",
+    //                     "type": "postback",
+    //                     "payload": "HELP_PAYLOAD"
+    //                 }
+    //             ]
+    //         }
+    //     ]
+    // }
+
+    var data = {
+        "greeting":[
+            {
+                "locale":"default",
+                "text":"Hello {{user_first_name}}!"
+            },{
+                "locale":"ar_AR",
+                "text":"مرحبا {{user_first_name}}!"
+            }
+        ]
+    }
+
+    request(
+        {
+            url : graph_api,
+            method : "POST",
+            headers : {
+                "Content-Type" : "application/json"
+            },
+            form:data
+        },
+        (err, response, body) => {
+            if (!err && response.statusCode === 200) {
+                console.log("Setup completed successfully.");
+                res.status(200).send("Setup completed successfully.");
+            } else {
+                console.error("Error setting up Messenger profile:", err || body);
+                res.status(500).send("Error setting up Messenger profile.");
+            }
+        }
+    )
+
+})
 
 // app.listen(3000, () => {
 //     console.log('Server running on port 3000');
